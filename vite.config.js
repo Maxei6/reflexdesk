7c6222448e32af46567dfb02e57e6a4648c2d9ad
Giorgio Croci <60924481+Maxei6@users.noreply.github.com>
2026-09-21 15:08:27 +0200

    Build Moonshine worker as ES module

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import { resolve } from "node:path";
 
 export default defineConfig({
+  worker: {
+    format: "es",
+  },
   build: {
+    target: "esnext",
     rollupOptions: {
       input: {
         main: resolve(import.meta.dirname, "index.html"),
